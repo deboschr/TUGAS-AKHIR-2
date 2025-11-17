10c495593ff539e3d184858426cc989f2b3ae665
deboschr <debospencher@gmail.com>
2025-11-17 23:00:55 +0700

    ubah nama folder views jadi windows

--- src/main/java/com/unpar/brokenlinkchecker/Application.java
+++ src/main/java/com/unpar/brokenlinkchecker/Application.java
@@ -53,7 +53,7 @@ public class Application extends javafx.application.Application {
         try {
             // Load layout utama dari file FXML
             FXMLLoader loader = new FXMLLoader(
-                    Application.class.getResource("/com/unpar/brokenlinkchecker/views/main-window.fxml"));
+                    Application.class.getResource("/com/unpar/brokenlinkchecker/windows/main-window.fxml"));
 
             // Buat scene baru untuk window ini
             Scene scene = new Scene(loader.load());
@@ -87,7 +87,7 @@ public class Application extends javafx.application.Application {
         try {
             // Load layout fxml untuk window link detail
             FXMLLoader loader = new FXMLLoader(
-                    Application.class.getResource("/com/unpar/brokenlinkchecker/views/link-window.fxml"));
+                    Application.class.getResource("/com/unpar/brokenlinkchecker/windows/link-window.fxml"));
 
             // Buat scene baru untuk window ini
             Scene scene = new Scene(loader.load());
@@ -126,7 +126,7 @@ public class Application extends javafx.application.Application {
         try {
             // Load layout notifikasi dari FXML
             FXMLLoader loader = new FXMLLoader(
-                    Application.class.getResource("/com/unpar/brokenlinkchecker/views/notification-window.fxml"));
+                    Application.class.getResource("/com/unpar/brokenlinkchecker/windows/notification-window.fxml"));
 
             // Buat scene baru untuk window ini
             Scene scene = new Scene(loader.load());
